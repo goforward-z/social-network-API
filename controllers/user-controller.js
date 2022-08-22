b14d65b592d943848898cb15a03156241000d8c1
goforward-z <zlepeng.ucf@gmail.com>
2022-08-21 21:21:34 -0400

    adda friend

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -47,6 +47,28 @@ const userController = {
         })
         .catch(err => res.status(400).json(err))
     },
+    //adding friend to friend list
+    addFriend({ params }, res) {
+        User.findOneAndUpdate(
+            { _id: params.id },
+            { $push: { friends: params.friendId }},
+            { new: true, runValidators: true }
+        )
+        .populate({
+            path: 'friends',
+            select: ('-__v')
+        })
+        .select('-__v')
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No User found with this id!' });
+                return;
+            }
+            res.json(dbUserData);
+            })
+            .catch(err => res.json(err));
+        
+    },
     //delete a user
     deleteUser({ params }, res) {
         User.findOneAndDelete({_id:params.id})
