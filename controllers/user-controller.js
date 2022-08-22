f61efb490f7c8aa54ed3f8a417f2903ae190ad65
goforward-z <zlepeng.ucf@gmail.com>
2022-08-21 20:10:57 -0400

    get all users

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,6 +1,17 @@
 const { User } = require('../models');
 
 const userController = {
+    //get all users
+    getAllUsers(req,res) {
+        User.find({})
+        .select('-__v')
+        .sort({_id: -1})
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    },
     //create a new user
     createUser ({ body }, res) {
         User.create(body)
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -2,12 +2,14 @@ const router = require('express'). Router();
 
 //importing controller functions
 const {
+    getAllUsers,
     createUser
 } = require('../../controllers/user-controller');
 
 // /api/users
 router 
 .route('/')
+.get(getAllUsers)
 .post(createUser);
 
 
