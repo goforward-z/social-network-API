2233a71522dfc5c2cca5d2e784e9050c32c02d97
goforward-z <zlepeng.ucf@gmail.com>
2022-08-21 21:54:28 -0400

    connecting thought route to server

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,12 +1,13 @@
 //connecting routes to the server
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
-const thoughtRoutes = require('')
+const thoughtRoutes = require('./thought-routes');
 
 
 //add prefix
 
 router.use('/users',userRoutes);
+router.use('/thoughts', thoughtRoutes);
 
 
 module.exports = router;
